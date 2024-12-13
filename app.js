9a290fcbf1da09c0c90364a1b12f12b65e26bb3b
bons <bons6710hos@gmail.com>
2024-12-13 21:15:00 +0300

    finish

---
+++ app.js
@@ -0,0 +1,19 @@
+// var logger=require('./logger')
+
+// console.log(logger)
+
+// logger.log('hello')
+
+// var mes= ""
+
+// console.log(global.mes)
+
+// console.log(module)  //print the module
+
+console.log(__filename);
+console.log(__dirname);
+
+
+var add = require('./logger')
+ 
+add.hello(3,7)
---
+++ logger.js
@@ -0,0 +1,18 @@
+
+// var  url = "www.github.com";
+
+// function log(message){
+
+
+
+// console.log(message)
+// }
+// module.exports.log = log
+console.log(__filename);
+console.log(__dirname);
+function hello(a,b)
+{
+    console.log(a+b);
+}
+module.exports.hello = hello;
+
