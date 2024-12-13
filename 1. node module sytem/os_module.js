f0caa7a1d2ab5ec530e281c6f59be2496c258393
bons <bons6710hos@gmail.com>
2024-12-13 21:30:30 +0300

    os module

---
+++ 1. node module sytem/os_module.js
@@ -0,0 +1,4 @@
+const os = require('os')
+ var freememo = os.freemem()
+
+console.log(freememo);
--- 1. node module sytem/path.js
+++ 1. node module sytem/path.js
@@ -1,3 +1,7 @@
 const path = require('path')
 
-console.log(path)
+var pathobj = path.parse(__filename)
+
+
+
+console.log(pathobj)
