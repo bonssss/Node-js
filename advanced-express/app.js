8aaa0fd905f606e9467d6fd754b1ee3d3d87f884
bons <bons6710hos@gmail.com>
2024-12-16 22:10:36 +0300

    NODE_ENV

--- advanced-express/app.js
+++ advanced-express/app.js
@@ -2,13 +2,24 @@ const express = require('express')
 const morgan= require('morgan')
 const app = express()
 const logger =require('./middleware')
+const { log } = require('console')
+
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+// console.log(`app:${app.get('env')}`);
+
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
 
 app.use(express.static('public'));
 
+
 app.use(logger)
 
+if(app.get('env') == "development")
+{
+  app.use(morgan('tiny'))  
+  console.log("Morgan is enabled")
+}
 app.use(function(req,res,next){
     console.log('Time:',new Date().toISOString())
     next()
