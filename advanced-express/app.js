5a02ef4dc6dc200008068a7e1e6200a513f43de4
bons <bons6710hos@gmail.com>
2024-12-18 21:23:54 +0300

    create debugger

--- advanced-express/app.js
+++ advanced-express/app.js
@@ -1,37 +1,39 @@
-const config =require('config')
-const express = require('express')
-const morgan= require('morgan')
-const app = express()
-const logger =require('./middleware')
-const { log } = require('console')
+const config = require("config");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const logger = require("./middleware");
+const { log } = require("console");
 
+const startupDebugger = require("debug")("app:startup");
+const dbdebugger = require("debug")("app:db");
 // console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
 // console.log(`app:${app.get('env')}`);
 
-app.use(express.json())
+app.use(express.json());
 // app.use(morgan('tiny'))
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 console.log("App name: " + config.get('name'))
 console,log("Description: " + config.get('mail.host'))
 
-app.use(logger)
+app.use(logger);
 
-if(app.get('env') == "development")
-{
-  app.use(morgan('tiny'))  
-  console.log("Morgan is enabled")
+if (app.get("env") == "development") {
+  app.use(morgan("tiny"));
+  //   console.log("Morgan is enabled")
+  startupDebugger("Morgan is enabled...");
 }
-app.use(function(req,res,next){
-    console.log('Time:',new Date().toISOString())
-    next()
+dbdebugger("DB connected")
+app.use(function (req, res, next) {
+  console.log("Time:", new Date().toISOString());
+  next();
+});
 
-})
-
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
-app.listen(3000,()=>{
-    console.log('server is running on port 3000')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
