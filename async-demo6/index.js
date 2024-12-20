8ded835286083994407bde91aab5a4bf0156f72a
bons <bons6710hos@gmail.com>
2024-12-20 20:30:11 +0300

    callback functions

--- async-demo6/index.js
+++ async-demo6/index.js
@@ -1,15 +1,33 @@
 console.log("First")
 
-setTimeout(() => {
-    console.log("wait for the seconds")
-    
-}, 2000);
+ Settime(1,function(user){
+    console.log("Second user" , user)
+ })
+
+ repo(1, (username)=>{
+    console.log("Second user" , username)
+ })
 
 console.log("Second");
 
+function Settime(id,callback){
+    setTimeout(() => {
+        console.log("wait for the seconds")
+        callback({id:id , name:"bonsss"})
+        // return {id:id , name:"bonsss"}
+    }, 2000);
+}
 
-setTimeout(() => {
-    console.log("wait for 0.2 sec");
+
+function repo(username,callback){
+    setTimeout(() => {
+        console.log("repo called")
+        callback (['repo1','repo2','repo3'])
+        
+    }, 200);
+}
+// setTimeout(() => {
+//     console.log("wait for 0.2 sec");
     
     
-}, 2000);
+// }, 2000);
