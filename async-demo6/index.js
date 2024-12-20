d795d46f70fe47c264ff40a05ad8f66e78d7b978
bons <bons6710hos@gmail.com>
2024-12-20 19:48:37 +0300

    Create async wait

---
+++ async-demo6/index.js
@@ -0,0 +1,15 @@
+console.log("First")
+
+setTimeout(() => {
+    console.log("wait for the seconds")
+    
+}, 2000);
+
+console.log("Second");
+
+
+setTimeout(() => {
+    console.log("wait for 0.2 sec");
+    
+    
+}, 2000);
