4eb7ed160b853617349f401f0743dde2a54f2a88
bons <bons6710hos@gmail.com>
2025-01-07 22:22:39 +0300

    minimize the lib code

--- testing/tests/lib.test.js
+++ testing/tests/lib.test.js
@@ -1,16 +1,18 @@
-const lib = require('../lib')
+const lib = require("../lib");
 
-test('result is +ve if number is +ve', () => { 
-const result= lib.absolute(2)
-expect(result).toBe(2)
- })
+describe("absolute", () => {
+  test("result is +ve if number is +ve", () => {
+    const result = lib.absolute(2);
+    expect(result).toBe(2);
+  });
 
- test('result is +ve if number is -ve', () => { 
-    const result= lib.absolute(-2)
-    expect(result).toBe(2)
-     })
+  test("result is +ve if number is -ve", () => {
+    const result = lib.absolute(-2);
+    expect(result).toBe(2);
+  });
 
-     test('result is 0 if number is 0', () => { 
-        const result= lib.absolute(0)
-        expect(result).toBe(0)
-         })
+  test("result is 0 if number is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+});
